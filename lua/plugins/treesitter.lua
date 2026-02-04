bc2603ac738cca44c94ccf4be4631d597fe3cf83
Wffv9FN <github_general_719@mcilvaney.co.uk>
2026-02-04 12:24:56 +0000

    treesitter fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,16 @@
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "lua", "vim", "bash", "python" }, -- extend as needed
-  highlight = {
-    enable = true,                -- Syntax highlighting
-    additional_vim_regex_highlighting = false,
-  },
-  indent = {
-    enable = true,                -- Improved indentation
-  },
-})
-
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "vim", "bash", "python" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
